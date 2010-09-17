54f6fbb387631b53b93c6f673933c397980cefe3
Jim Safley <jsafley@gmu.edu>
2010-09-17 17:55:03 -0400

    Modifies the example adapter to be more helpful for testing.

--- Adapter/Example.php
+++ Adapter/Example.php
@@ -6,30 +6,109 @@
 
 require_once 'Scripto/Adapter/Interface.php';
 
+/**
+ * An example adapter for a hypothetical CMS.
+ * 
+ * @see Scripto_Adapter_Interface
+ */
 class Scripto_Adapter_Example implements Scripto_Adapter_Interface
 {
-    private $_pages = array('1' => 'Cover Page',
-                            '2' => 'Introduction: page 1', 
-                            '3' => 'Introduction: page 2');
+    /**
+     * Example document data.
+     * 
+     * For example purposes the document data are stored following this format:
+     * 
+     * {documentId} => array(
+     *     {pageId} => array(
+     *         'page_name' => {pageName}, 
+     *         'page_image_url' => {pageImageUrl}
+     *     )
+     * ) 
+     * 
+     * Other adapters will likely get relevant data using the CMS API, and not 
+     * hardcode them like this example. Be sure to URL encode the document and 
+     * page IDs when transporting over HTTP. For example:
+     * 
+     * documentId: Request for Purchase of Liver Oil & Drum Heads
+     * pageId: xbe/XBE02001.jpg
+     * ?documentId=Request+for+Purchase+of+Liver+Oil+%26+Drum+Heads&pageId=xbe%2FXBE02001.jpg
+     * 
+     * These example documents are from CHNM's Papers of the War Department.
+     * 
+     * @var array
+     */
+    private $_documents = array(
+        // Example of the preferred way to set the document and page IDs using 
+        // unique keys.
+        // See: http://wardepartmentpapers.org/document.php?id=16344
+        16344 => array(
+            67799 => array(
+                'page_name' => 'Letter Outside', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/zto/ZTO07001.jpg'
+            ), 
+            67800 => array(
+                'page_name' => 'Letter Body', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/zto/ZTO07002.jpg'
+            ), 
+            67801 => array(
+                'page_name' => 'Worksheet 1, Outside', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/zto/ZTO07003.jpg'
+            ), 
+            67802 => array(
+                'page_name' => 'Worksheet 1, Page 1', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/zto/ZTO07004.jpg'
+            ), 
+            67803 => array(
+                'page_name' => 'Worksheet 1, Page 2', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/zto/ZTO07005.jpg'
+            ), 
+            67804 => array(
+                'page_name' => 'Worksheet 2, Outside', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/zto/ZTO07006.jpg'
+            ), 
+            67805 => array(
+                'page_name' => 'Worksheet 2, Page 1', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/zto/ZTO07007.jpg'
+            )
+        ), 
+        // Alternate way to set document and page IDs using all possible 
+        // characters.
+        // See: http://wardepartmentpapers.org/document.php?id=41827
+        'Request for Purchase of Liver Oil & Drum Heads' => array( // document ID
+            'xbe/XBE02001.jpg' => array( // page ID
+                'page_name' => '1', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/xbe/XBE02001.jpg'
+            ), 
+            // page ID
+            'xbe/XBE02002.jpg' => array( // page ID
+                'page_name' => '2', 
+                'page_image_url' => 'http://wardepartmentpapers.org/images/medium/xbe/XBE02002.jpg'
+            )
+        )
+    );
     
     public function documentExists($documentId)
     {
-        return true;
+        return array_key_exists($documentId, $this->_documents);
     }
     
     public function documentPageExists($documentId, $pageId)
     {
-        return array_key_exists($pageId, $this->_pages);
+        return array_key_exists($pageId, $this->_documents[$documentId]);
     }
     
     public function getDocumentPages($documentId)
     {
-        return $this->_pages;
+        $pages = array();
+        foreach ($this->_documents[$documentId] as $pageId => $page) {
+            $pages[$pageId] = $page['page_name'];
+        }
+        return $pages;
     }
     
     public function getDocumentPageImageUrl($documentId, $pageId)
     {
-        return 'http://upload.wikimedia.org/wikipedia/commons/e/e6/Sargis_Pitsak.jpg';
+        return $this->_documents[$documentId][$pageId]['page_image_url'];
     }
     
     public function importDocumentPageTranscription($documentId, $pageId, $text)
