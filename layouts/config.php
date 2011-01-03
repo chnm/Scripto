0d662a81ba7af9bf8a9b2eccf02fde2925d98491
Ken <kalbers@gmu.edu>
2011-01-03 10:15:07 -0500

    Fix overflow issues.

--- layouts/config.php
+++ layouts/config.php
@@ -2,16 +2,16 @@
 ini_set('display_errors', 1);
 
 // Path to directory containing Zend Framework, from root.
-define('ZEND_PATH', '');
+define('ZEND_PATH', '/Users/ken/Sites/omeka-trunk/application/libraries/');
 
 // Path to directory containing the Scripto library, from root.
-define('SCRIPTO_PATH', '');
+define('SCRIPTO_PATH', '/Users/ken/Sites/');
 
 // URL to the MediaWiki installation API.
-define('MEDIAWIKI_API_URL', '');
+define('MEDIAWIKI_API_URL', 'http://localhost/~ken/mediawiki/api.php');
 
 // Name of the MediaWiki database.
-define('MEDIAWIKI_DB_NAME', '');
+define('MEDIAWIKI_DB_NAME', 'wikidb');
 
 // Set the include path to Zend and Scripto libraries.
 set_include_path(get_include_path() 
